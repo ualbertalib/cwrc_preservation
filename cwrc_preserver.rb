2582749fb25b16203a95e522e3ae41627cdea492
astrilet <astrilet@gmail.com>
2017-11-08 15:48:08 -0700

    Almost there, just need to add input start datetime

--- cwrc_preserver.rb
+++ cwrc_preserver.rb
@@ -9,119 +9,126 @@ require 'swift_ingest'
 
 module CWRCPerserver
 
-  class ErrorConnetingToCWRC < StandardError; end
+  class CWRCArchivingError < StandardError; end
 
-  # read secrets.yml and set up environment vars
-  env_file = './secrets.yml'
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
+  def self.get_cookie()
 
-  # create swift object
-  swift_depositer = SwiftIngest::Ingestor.new(username: 'test:tester',
-                                              password: 'testing',
-                                              tenant: 'tester',
-                                              auth_url: 'http://127.0.0.1:8080/auth/v1.0',
-                                              project: 'ERA')
+    # Initialize a cookie jar
+    jar = HTTP::CookieJar.new
 
-  #swift_depositer = SwiftIngest::Ingestor.new(username: ENV['SWIFT_USERNAME'],
-  #                                            password: ENV['SWIFT_PASSWORD'],
-  #                                            tenant: ENV['SWIFT_TENANT'],
-  #                                            auth_url: ENV['SWIFT_AUTH_URL'],
-  #                                            project_name: ENV['SWIFT_PROJECT_NAME'],
-  #                                            project_domain_name: ENV['SWIFT_PROJECT_DOMAIN_NAME'],
-  #                                            project: ENV['SWIFT_PROJECT'])
+    cookie_file = 'connection_cookie.txt'
+    if File.exist?(cookie_file)
+      jar.load(cookie_file)
+      cookie = jar.cookies("https://#{@cwrc_hostname}")[0]
+      return cookie.cookie_value unless cookie.expired? || cookie.nil?
+    end
 
-  # login into cwrc and get cookie
-  cwrc_server = 'https://cwrc-dev-05.srv.ualberta.ca'
-  cwrc_login_path='/rest/user/login'
-  login_uri = URI.parse("#{cwrc_server}#{cwrc_login_path}")
-  login_request = Net::HTTP::Post.new(login_uri)
-  login_request.content_type = "application/json"
-  login_request.body = JSON.dump({
-    "username" => "ualbertalib",
-    "password" => "m2ey8V2xnJM22kiN"
-  })
+    login_request = Net::HTTP::Post.new(URI.parse("https://#{@cwrc_hostname}#{@cwrc_login_path}"))
+    login_request.content_type = "application/json"
+    login_request.body = JSON.dump({
+                                       "username" => "ualbertalib",
+                                       "password" => "m2ey8V2xnJM22kiN"
+                                   })
+    login_response = Net::HTTP.start(@cwrc_hostname, @cwrc_port, @cwrc_options) do |http|
+      http.request(login_request)
+    end
 
-  req_options = {
-    use_ssl: login_uri.scheme == "https",
-  }
+    # Check response code
+    raise CWRCArchivingError unless login_response.code.to_s == "200"
 
-  login_response = Net::HTTP.start(login_uri.hostname, login_uri.port, req_options) do |http|
-        http.request(login_request)
+    login_response.get_fields('Set-Cookie').each do |value|
+      jar.parse(value, "https://#{@cwrc_hostname}")
+    end
+
+    jar.save(cookie_file)
+
+    HTTP::Cookie.cookie_value(jar.cookies("https://#{@cwrc_hostname}"))
   end
 
-  # Check response code
-  raise ErrorConnetingToCWRC unless login_response.code.to_s == "200"
-
-  # Initialize a cookie jar
-  jar = HTTP::CookieJar.new
-
-  login_response.get_fields('Set-Cookie').each do |value|
-    jar.parse(value, cwrc_server)
+  def self.set_env()
+    # read secrets.yml and set up environment vars
+    env_file = './secrets.yml'
+    raise CWRCArchivingError if !File.exists?(env_file)
+    YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end
   end
 
-  # Save to a file
-  jar.save('connection_cookie.txt')
-
-
-  # Load from a file
-  # jar.load(filename) if File.exist?(filename)
-
-  # fetch json file and break it into individual object
-  cwrc_all_objects_path='/services/bagit_extension/audit'
-  all_obj_uri = URI.parse("#{cwrc_server}#{cwrc_all_objects_path}")
-  all_obj_req = Net::HTTP::Get.new(all_obj_uri)
-  all_obj_req['Cookie'] = HTTP::Cookie.cookie_value(jar.cookies(cwrc_server))
-  all_obj_req_options = {
-      use_ssl: all_obj_uri.scheme == "https",
-  }
-
-  all_obj_response = Net::HTTP.start(all_obj_uri.hostname, all_obj_uri.port, all_obj_req_options) do |http|
-    http.request(all_obj_req)
+  def self.get_cwrc_objs(cookie)
+    all_obj_uri = URI.parse("https://#{@cwrc_hostname}/services/bagit_extension/audit")
+    all_obj_req = Net::HTTP::Get.new(all_obj_uri)
+    all_obj_req['Cookie'] = cookie
+    all_obj_response = Net::HTTP.start(@cwrc_hostname, @cwrc_port, @cwrc_options) do |http|
+      http.request(all_obj_req)
+    end
+    (JSON.parse(all_obj_response.body))['objects']
   end
 
-  cwrc_objs= (JSON.parse(all_obj_response.body))['objects']
-
-
-  # for each object
-  cwrc_objs.each do |cwrc_obj|
-
+  def self.download_cwrc_obj(cookie, cwrc_obj, cwrc_file)
     #download object from cwrc
-    obj_uri = URI.parse("https://cwrc-dev-05.srv.ualberta.ca/islandora/object/#{cwrc_obj['pid']}/manage/bagit_extension")
-    obj_req = Net::HTTP::Get.new(obj_uri)
-
-    obj_req['Cookie'] = HTTP::Cookie.cookie_value(jar.cookies(cwrc_server))
-    obj_req_options = {
-        use_ssl: obj_uri.scheme == "https",
-    }
-    obj_response = Net::HTTP.start(obj_uri.hostname, obj_uri.port, obj_req_options) do |http|
+    obj_path = "https://#{@cwrc_hostname}/islandora/object/#{cwrc_obj['pid']}/manage/bagit_extension"
+    obj_req = Net::HTTP::Get.new(URI.parse(obj_path))
+    obj_req['Cookie'] = cookie
+    obj_response = Net::HTTP.start(@cwrc_hostname, @cwrc_port, @cwrc_options) do |http|
       http.request(obj_req)
     end
 
-    cwrc_file = "#{cwrc_obj['pid'].to_s.tr(':', '_')}.zip"
     open(cwrc_file, "wb") do |file|
       file.write(obj_response.body)
     end if obj_response.code.to_s == '200'
+   end
 
 
-    #deposit into swift
-    swift_depositer.deposit_file(cwrc_file, 'CWRC')
+  # set environment
+  set_env
+
+  # cwrc login credentials
+  @cwrc_hostname = ENV['CWRC_HOSTNAME']
+  @cwrc_login_path = ENV['CWRC_LOGIN_PATH']
+  @cwrc_port = ENV['CWRC_PORT'].to_s.to_i
+  @cwrc_options = {
+      use_ssl: true
+  }
+  cwrc_swift_container = ENV['CWRC_SWIFT_CONTAINER']
+
+  # get connection cookie
+  cookie = get_cookie()
+
+
+  # connect to swift storage
+  swift_depositer = SwiftIngest::Ingestor.new(username: ENV['SWIFT_USERNAME'],
+                                              password: ENV['SWIFT_PASSWORD'],
+                                              tenant: ENV['SWIFT_TENANT'],
+                                              auth_url: ENV['SWIFT_AUTH_URL'],
+                                              project_name: ENV['SWIFT_PROJECT_NAME'],
+                                              project_domain_name: ENV['SWIFT_PROJECT_DOMAIN_NAME'],
+                                              project: ENV['SWIFT_PROJECT'])
+
+  raise CWRCArchivingError if swift_depositer.nil?
+
+  # get list of all objects from cwrc
+  cwrc_objs=  get_cwrc_objs(cookie)
+
+  # for each cwrc object
+  cwrc_objs.each do |cwrc_obj|
+
+    cwrc_file = "#{cwrc_obj['pid'].to_s.tr(':', '_')}.zip"
+
+    # check if file has been deposited
+    swift_file = swift_depositer.get_file_from_swit(cwrc_file, cwrc_swift_container )
+
+    # if object is not is swift or we have newer object
+    if swift_file.nil? || cwrc_obj['timestamp'].to_s.to_time > swift_file.metadata['timestamp'].to_s.to_time
+
+      #download object from cwrc
+      download_cwrc_obj(cookie, cwrc_obj, cwrc_file)
+      raise CWRCArchivingError if !File.exist?(cwrc_file)
+
+      # deposit into swift an remove it
+      swift_depositer.deposit_file(cwrc_file, cwrc_swift_container, {timestamp: cwrc_obj['timestamp'].to_s})
+      FileUtils.rm_rf(cwrc_file) if File.exist?(cwrc_file)
+    end
 
   end
-
-
-
-
-  #    response = Net::HTTP.get_response(uri)
-  # check if it is already in swift
-  # if no
-  #    download object from cwrc
-  #    uri = URI.parse("https://cwrc-dev-05.srv.ualberta.ca/islandora/object/orlando:1bdcd4f4-11f5-4bb0-b6a4-5caaaca2079a/manage/bagit_extension")
-  #    response = Net::HTTP.get_response(uri)
-  #
-  #    check checksum of the download object against json header
-  #    verify bag with bagit gem
-  #    ingest object into swift
 
 end
